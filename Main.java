9194d13b170a3311b11d74a5111de5c4927694b5
MohammedZ666 <50874919+MohammedZ666@users.noreply.github.com>
2020-02-22 11:04:26 +0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,9 +1,11 @@
+package Lab003;
+
 import java.io.File; 
 import java.io.FileNotFoundException;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner; 
-public class Main
+public class Task02
 {
 public static final int WHITE = 0;
 public static final int GRAY = 1;
@@ -11,14 +13,14 @@ public static final int BLACK = 2;
 public static void main(String[] args) throws FileNotFoundException 
 { 
 	// pass the path to the file as a parameter 
-	String file_dir = "C:\\Users\\18201214\\Desktop\\New folder\\src\\graph.txt";
+	String file_dir = "graph.txt";
 	File file = new File(file_dir); 
 	Scanner sc = new Scanner(file); 
-        int vertices = sc.nextInt() + 1;
+        int vertices = sc.nextInt();
         int [][] adjMat = new int[vertices][vertices]; 
        	while (sc.hasNextInt()) {
-            int i = sc.nextInt();
-            int j = sc.nextInt();
+            int i = sc.nextInt()-1;
+            int j = sc.nextInt()-1;
             adjMat[i][j]++;
             adjMat[j][i]++;
             
@@ -35,7 +37,7 @@ public static void BFS(int[][]G , int s){
 	Object[] parent = new Object[G[0].length];
 	
 	int []distance = new int[G[0].length];
-	for(int i = 1;i<G[0].length;i++)distance[i] = -1;
+	for(int i = 1;i<G[0].length;i++)distance[i] = 0;
 	
 		
 	colors[s] = GRAY;
@@ -46,10 +48,9 @@ public static void BFS(int[][]G , int s){
 	Q.add(s);
 	
 	while(!Q.isEmpty()){
-		
 		int u = Q.remove();
-		for(int v = 1; v<G[0].length; v++){
-			
+		for(int v = 0; v<G[0].length; v++){
+			if(G[u][v] == 1){
 			if(colors[v]==WHITE){
 				
 				colors[v] = GRAY;
@@ -58,13 +59,17 @@ public static void BFS(int[][]G , int s){
 				Q.add(v);
 				
 			}
+                       }
 		colors[u] = BLACK;	
 		}
 	}
 	
 	
-for(int color:colors)System.out.println(color);	
-for(int d:distance)System.out.println("1"+d);
-for(Object p:parent)System.out.println("2"+p);
+for(int color:colors)System.out.print(color+",");
+        System.out.println();	
+for(int d:distance)System.out.print(d+", ");
+        System.out.println();	
+
+for(Object p:parent)System.out.print(p+",");
 }
 }
